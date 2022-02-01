ad353f92e617d4f02eb451694b3cfb57af6e3b5a
Ben Hunter <code@benhunter.me>
2022-02-01 07:21:22 -0600

    Refactor components to separate files

---
+++ src/Components/Board.js
@@ -0,0 +1,35 @@
+import React from 'react';  // needed for class components, not functional components
+import Square from "./Square";  // importing from a local file
+
+function Board(props) {
+    function renderSquare(i) {
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => props.onClick(i)}
+            />
+        );
+    }
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+export default Board;
